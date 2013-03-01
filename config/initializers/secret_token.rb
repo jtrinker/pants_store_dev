488971e2c45f48ac6979d0543b602ba0ce653600
Jameson Trinker <jameson.trinker@gmail.com>
2013-03-01 15:22:24 -0600

    Pants Store site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PantsStoreDev::Application.config.secret_token = 'f1ce6598ce9408947ae64c3e0e1e26b52a499083c13fa7585c3e66d7267b2d964c3a67456542c6a8a87c8f5fb8362df269c37769e067f337c32d192bf1fa895c'
